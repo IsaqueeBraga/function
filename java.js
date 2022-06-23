7df7d46b3cfce2b81b3be31ac00d1809579a94dc
Isaquee Braga <flaisaque@hotmail.com>
2022-06-23 12:09:58 -0300

    aula 04

---
+++ java.js
@@ -0,0 +1,24 @@
+function Time(oi){
+    console. log ("Flamengo é o maior")
+}
+Time ("t1 é a melhor turma")
+
+function nome (Isaque){
+    console. log (`Eu me chamo ${Isaque}`)
+
+}
+nome ("Isaque")
+
+
+function estilo (nome, numero, musica, cidade, filme, perfect){
+    console. log (`Meu nome é ${nome},minha idade é ${numero}, meu estilo musical é ${musica}, moro no ${cidade}, meu filme favorito é ${filme}, musica predileta ${perfect}`)
+}
+estilo ("Isaque", 23, "rap","Rio de Janeiro", "Vingadores", "L7 Celebrando a vida")
+
+
+
+let impares = 3;
+while (50 >= impares){
+    console. log (impares)
+    impares = impares +3
+}
